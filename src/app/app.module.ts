77c982b7b9c2b9a34ca6a94a75ed73efd4c564c7
paolo palmerini <paolo.palmerini@gmail.com>
2024-05-19 11:38:54 +0200

    correzioni varie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [
-    HttpClientModule,
-    // altre importazioni...
+  declarations: [
+    AppComponent,
+    FileUploadComponent
   ],
-  // ...
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
 
